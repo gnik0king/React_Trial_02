0b747f6ea9bf0402689d409b2f9fab2926bd88e7
Thomas Wangia <tuwangia@outlook.com>
2023-01-17 18:37:35 -0800

    Setting up API

--- SpiffosList/api/views.py
+++ SpiffosList/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from .serializers import TodoSerializer
 
+from .models import Todo
 # Create your views here.
+
+class TodoListView(generics.ListAPIView):
+    model = Todo
+    serializer_class = TodoSerializer
